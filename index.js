8619c7760f54e3c43d9552761a104711dcae9abf
obayedullah <obayedullahkhan986@gmail.com>
2023-12-22 19:27:28 +0600

    project done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const port = process.env.PORT || 5000;
@@ -14,103 +13,119 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtnim2c.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const taskCollection = client.db("tasksHub").collection("tasks")
         const completeTaskCollection = client.db("tasksHub").collection("completeTasks")
         const commentsCollection = client.db("tasksHub").collection("comments")
 
 
-        app.post("/tasks", async(req, res) =>{
+        app.post("/tasks", async (req, res) => {
             const tasks = req.body;
             const result = await taskCollection.insertOne(tasks);
             res.send(result);
         });
 
-        app.post("/completeTasks", async(req, res) =>{
+        app.get("/createdTasks", async (req, res) => {
+            const query = {};
+            const createdTask = taskCollection.find(query);
+            const result = await createdTask.toArray();
+            res.send(result);
+        })
+
+        app.post("/completeTasks", async (req, res) => {
             const completeTask = req.body;
             const result = await completeTaskCollection.insertOne(completeTask);
             res.send(result);
         });
 
-        app.post("/comments", async(req, res) =>{
+        app.post("/comments", async (req, res) => {
             const comments = req.body;
             const result = await commentsCollection.insertOne(comments);
             res.send(result);
         });
 
-        app.get("/tasks", async(req, res) =>{
+        app.get("/tasks", async (req, res) => {
             const email = req.query.email;
-            const query = {email};
+            const query = { email };
             const result = await taskCollection.find(query).toArray();
             res.send(result);
-        });                                 
+        });
 
-        app.get("/completeTasks", async(req, res) =>{
-            const email = req.query.email; 
-            const query = {email};
+        app.get("/completeTasks", async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
             const result = await completeTaskCollection.find(query).toArray()
             res.send(result);
         });
 
-        app.get("/tasks/:id", async(req, res) =>{
+        app.get("/tasks/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id) }
+            const query = { _id: ObjectId(id) }
             const tasks = await taskCollection.findOne(query)
             res.send(tasks);
         });
 
-        app.get("/comments", async(req, res) =>{
+        app.get("/comments", async (req, res) => {
             const query = {};
             const comments = await commentsCollection.find(query).toArray();
             res.send(comments);
         })
 
-        app.put("/updateTasks/:id", async(req, res) =>{
+        app.put("/updateTasks/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const task = req.body;
-            const option = {upsert: true}
+            const option = { upsert: true }
             const updatedTask = {
-                $set:{
-                   title: task.title,
-                   details: task.details,
+                $set: {
+                    title: task.title,
+                    details: task.details,
                 }
             }
             const result = await taskCollection.updateOne(query, updatedTask, option)
             res.send(result);
         })
 
-        app.delete("/tasks/:id", async(req, res) =>{
+        app.delete("/tasks/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await taskCollection.deleteOne(query);
             res.send(result);
         })
 
-        app.delete("/completeTasks/:id", async(req, res) =>{
+        app.delete("/deleteCompleteTasks/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await completeTaskCollection.deleteOne(query);
             res.send(result);
-        }); 
+        });
 
 
-        app.delete("/deleteTask/:id", async(req, res) =>{
+        app.delete("/deleteTask/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}; 
-            const result = await completeTaskCollection.deleteMany(query);
-            res.send(result);
+            const query = { _id: ObjectId(id) }; 
+
+
+                const taskResult = await taskCollection.deleteOne(query); 
+                const completeTaskResult = await completeTaskCollection.deleteMany(query);
+         
+                const result = {
+                    task: taskResult,
+                    completeTask: completeTaskResult,
+                };
+        
+                res.send(result);
         })
 
-        
+
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -119,12 +134,11 @@ run().catch(error => console.error(error))
 
 
 
-app.get("/", async(req, res) =>{
+app.get("/", async (req, res) => {
     res.send("Tasks web server is running")
 });
 
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`The server is running on port ${port}`)
 })
- 
